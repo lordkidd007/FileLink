e7e1d5961025414f75976fff9de8798b604e8c3b
杨鹏赫 <yangpenghe@vip.qq.com>
2021-12-10 08:11:02 +0800

    '2021-12-10-08:11:02'

--- toLink.py
+++ toLink.py
@@ -72,7 +72,7 @@ if(strRes != ""):
     cmd = "base64 -w 0 node.txt > v2.txt"
     os.system(cmd)
     strTime = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
-    os.system("echo <h1>%s</h1> > index.html" % strTime)
+    os.system("echo <h1\>%s</h1\> > index.html" % strTime)
     os.system("git add .")
     os.system("git commit -m '%s'" %(strTime) )
     os.system("git push")
