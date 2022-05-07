1d3d4418fd139b24a1156e6f38949f3a49a7c86f
杨鹏赫 <yangpenghe@vip.qq.com>
2022-05-07 20:25:41 +0800

    '2022-05-07-20:25:41'

---
+++ 1.js
@@ -0,0 +1,57 @@
+-		m_Cir	{_FirstAxis={...} _SecondAxis={...} _Center={...} ...}	CATMathCircle
++		CATMathCurve	{...}	CATMathCurve
++		_FirstAxis	{_x=-1.0000000000000000 _y=0.00000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_SecondAxis	{_x=0.00000000000000000 _y=1.0000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_Center	{_x=-2100.0000000000000 _y=700.00000000000000 _z=0.00000000000000000 }	CATMathPoint
+        _Radius	20.000000000000000	double
+        _Scale	1.0000000000000000	double
+        _Shift	0.00000000000000000	double
+        iThis	0.00000000000000000	double
+-		p2	{_x=-2120.0000000000000 _y=700.00000000000000 _z=0.00000000000000000 }	CATMathPoint
+_x	-2120.0000000000000	double
+_y	700.00000000000000	double
+_z	0.00000000000000000	double
+-		m_Cir	{_FirstAxis={...} _SecondAxis={...} _Center={...} ...}	CATMathCircle
++		CATMathCurve	{...}	CATMathCurve
++		_FirstAxis	{_x=-1.0000000000000000 _y=0.00000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_SecondAxis	{_x=0.00000000000000000 _y=1.0000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_Center	{_x=-2100.0000000000000 _y=700.00000000000000 _z=800.00000000000000 }	CATMathPoint
+        _Radius	20.000000000000000	double
+        _Scale	1.0000000000000000	double
+        _Shift	0.00000000000000000	double
+        iThis	0.00000000000000000	double
+-		p2	{_x=-2120.0000000000000 _y=700.00000000000000 _z=800.00000000000000 }	CATMathPoint
+        _x	-2120.0000000000000	double
+        _y	700.00000000000000	double
+        _z	800.00000000000000	double
+
+
+
+                -		m_Cir	{_FirstAxis={...} _SecondAxis={...} _Center={...} ...}	CATMathCircle
++		CATMathCurve	{...}	CATMathCurve
++		_FirstAxis	{_x=0.00000000000000000 _y=-1.0000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_SecondAxis	{_x=-1.0000000000000000 _y=0.00000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_Center	{_x=-2100.0000000000000 _y=700.00000000000000 _z=0.00000000000000000 }	CATMathPoint
+        _Radius	20.000000000000000	double
+        _Scale	1.0000000000000000	double
+        _Shift	0.00000000000000000	double
+        iThis	1.0000000000000000	double
+-		p2	{_x=-2100.0000000000000 _y=720.00000000000000 _z=0.00000000000000000 }	CATMathPoint
+        _x	-2100.0000000000000	double
+        _y	720.00000000000000	double
+        _z	0.00000000000000000	double
+
+                        
+                        -		m_Cir	{_FirstAxis={...} _SecondAxis={...} _Center={...} ...}	CATMathCircle
++		CATMathCurve	{...}	CATMathCurve
++		_FirstAxis	{_x=0.00000000000000000 _y=-1.0000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_SecondAxis	{_x=-1.0000000000000000 _y=0.00000000000000000 _z=0.00000000000000000 }	CATMathVector
++		_Center	{_x=-2100.0000000000000 _y=700.00000000000000 _z=800.00000000000000 }	CATMathPoint
+		_Radius	20.000000000000000	double
+		_Scale	1.0000000000000000	double
+		_Shift	0.00000000000000000	double
+		iThis	1.0000000000000000	double
+-		p2	{_x=-2100.0000000000000 _y=720.00000000000000 _z=800.00000000000000 }	CATMathPoint
+		_x	-2100.0000000000000	double
+		_y	720.00000000000000	double
+		_z	800.00000000000000	double
